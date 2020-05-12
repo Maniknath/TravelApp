4b7187d66b805dc75b893321775c63819baad6b9
maniknath <maniknath79@gmail.co>
2020-05-12 12:01:06 -0400

    first mobile app

---
+++ TravelRecordApp/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Xamarin.Forms;
+
+namespace TravelRecordApp
+{
+    public partial class App : Application
+    {
+        public static string DatabaseLocation = string.Empty;
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        public App(string databaseLocation)
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+            DatabaseLocation = databaseLocation;
+        }
+
+        protected override void OnStart()
+        {
+            //handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            //handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            //handle when your app resumes
+        }
+    }
+}
---
+++ TravelRecordApp/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace TravelRecordApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Clicked(object sender, EventArgs e)
+        {
+            bool isEmailEmpty = string.IsNullOrEmpty(emailEntry.Text);
+            bool isPasswordEmpry = string.IsNullOrEmpty(passwordEntry.Text);
+
+            if (isEmailEmpty || isPasswordEmpry)
+            {
+
+            }
+            else
+            {
+                Navigation.PushAsync(new HomePage());
+            }
+        }
+    }
+}
